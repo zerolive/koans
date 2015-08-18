c7a108afbc63972182933cf13404e86cee73ef32
zerolive <zerolive@gmail.com>
2015-08-18 14:25:04 +0200

    On about_extra_credit.rb añadida tirada

--- about_extra_credit.rb
+++ about_extra_credit.rb
@@ -6,3 +6,26 @@
 # You already have a DiceSet class and score function you can use.
 # Write a player class and a Game class to complete the project.  This
 # is a free form assignment, so approach it however you desire.
+
+ class DiceSet
+  attr :values
+  def roll (number)
+    @values = []
+    while number > 0 do
+      number = number - 1
+      @values.push(1 + rand(6))
+    end
+  end
+ end
+ player1 = DiceSet.new
+ player2 = DiceSet.new
+ player1.roll(5)
+ player2.roll(5)
+
+class score
+	attr :points
+	def calcu
+end
+
+ p "Jugador 1: #{player1.values}"
+ p "Jugador 2: #{player2.values}"
