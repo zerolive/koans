1e991a31b0e3c7b5ccfe5f4c514d6b721582ebe4
zerolive <zerolive@gmail.com>
2015-08-24 13:45:36 +0200

    About_java_interop.rb solved

--- path_to_enlightenment.rb
+++ path_to_enlightenment.rb
@@ -36,6 +36,6 @@ require 'about_message_passing'
 #require 'about_proxy_object_project'
 require 'about_to_str'
 in_ruby_version("jruby") do
-  #require 'about_java_interop'
+  require 'about_java_interop'
 end
 require 'about_extra_credit'
