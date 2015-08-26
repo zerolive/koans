45e9fbe28ffe641097e34d459ac0bfae600be4a1
zerolive <zerolive@gmail.com>
2015-08-26 14:39:22 +0200

    creando mi propio koan

--- about_my_own_koan.rb
+++ about_my_own_koan.rb
@@ -4,20 +4,19 @@ class MyOwnKoan < Neo::Koan
   
   #FIX DIFERENTS ERRORS
   #Fix this ArgumentError. maybe we have many numbers
-  #[1, 2, 3].first(4, 6)
-  
+  [1, 2, 3].first(4, 6)
 
   #Fix this SystaxError. Could be eval doesn't take some simbol
-  #eval(1+1=2)
+  eval(1+1=2)
 
   #Fix this NameError, I missed something
-  #__ = "wag"
-  #puts dog
+  __ = "wag"
+  puts dog
 
   #Fix this TypeError. Integers and strings, I don't know.
-  #[1, 2, 3].first("two")
+  [1, 2, 3].first("two")
 
   #Fix this NoMethodError. Could be to_array does't exist
-  #dog = "wag".to_array
+  dog = "wag".to_array
 
 end
